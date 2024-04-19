f064472763d9af2c0359807cf20e095903a93eb4
Roma <roman.kasatnik@gmail.com>
2024-04-19 16:59:38 +0300

    .

--- News.py
+++ News.py
@@ -1,10 +1,13 @@
 import random
 import json
 import time
+from pprint import pprint
+
 import telebot as tel
 import wikipedia
 import requests
 import Tokens
+from newsapi import NewsApiClient
 
 token = Tokens.TOKEN_NEWS
 creator = Tokens.CREATOR
@@ -20,6 +23,31 @@ def start(message):
                      "Привет! Это бот о новостях")
 
 
+@bot.message_handler(commands=["send_admin"])
+def send_admin(message):
+    t = message.text.split()
+    v = " ".join(t[2:])
+    g = t[1]
+    print(g, v)
+    try:
+        bot.send_message(g, v)
+        # bot.send_message(message.from_user.id, f"Обработано, всё красиво")
+    except tel.apihelper.ApiTelegramException as Error:
+        bot.send_message(message.from_user.id, f"Обработано, {Error}")
+
+
+def news_api(request):
+    # newsapi = NewsApiClient(api_key=Tokens.NEWS_API)
+    # top_headlines = newsapi.get_top_headlines(q='Майнкрафт',
+    #                                           language='ru')
+    # print(top_headlines)
+
+    answer = requests.get(f"https://newsapi.org/v2/everything?q={request}&language=ru&apiKey={Tokens.NEWS_API}")
+    pprint(answer.json())
+    x = answer.json()
+
+news_api("Майнкрафт")
+
 
 def open_file_r(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -34,12 +62,11 @@ def open_file_w(file_path, data):
 
 bot.polling()
 
-
 """
-Сделать команду для админа, которая поможет отправлять сообщения пользователям.
-Создатель пишет: "/send 38547387 привет, как твои дела?" - это сообщение будет срабатывать по команде /send
-Бот должен разделить команду /send, затем айдишник и само сообщение с помощью функции split(), то есть, в таком виде:
-["/send", "3458934873", "привет..."] - вот такой результат должен быть
-Мы достаём айдишник из списка и просто юзаем bot.send_message...
+Юзеру отправлять сообщение мы научились, теперь нужно сделать наоборот (по какой-то команде отправить сообщение создателю), подсказка: есть функция forward_message 
 
+2. Достать красиво все данные из переменной x и отправить их в ответ на сообщение отправителя.
+Все новости должны быть запакованы в одну переменную (это важно, чтобы не бот не спамил юзеру)
+
+3. Обрабатывать все ошибки в процессе
 """
